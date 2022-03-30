c7f8792dd431a4b3e621869c9e4591cd06034adf
Don Wijesinghe <menuwan.wijesinghe@gmail.com>
2022-03-30 17:43:30 -0500

    Initial Commit

---
+++ CardProject/CardProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardProjectApp.swift
+//  CardProject
+//
+//  Created by Don wijesinghe on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CardProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardProject
+//
+//  Created by Don wijesinghe on 3/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
